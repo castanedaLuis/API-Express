c65adcc1749dcff04e3c0ef0b011f3d3e1c4d62b
castanedaLuis <castanedaLuis@github.com>
2023-02-03 01:46:46 -0600

    [🔥] validando los datos que se ingresan por medio del body, params o querys con Joi

--- middlewares/error.handler.js
+++ middlewares/error.handler.js
@@ -1,12 +1,12 @@
 
 function logErrors (err, req, res, next) {
-    console.log('logErrors');
+    //console.log('logErrors');
     console.error(err);
     next(err);
   }
   
   function errorHandler(err, req, res, next) {
-    console.log('errorHandler');
+    //console.log('errorHandler');
     res.status(500).json({
       message: err.message,
       stack: err.stack,
