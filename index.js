caac57e8ff7f111748cdcf5308f41a174c9dc9fe
castanedaLuis <castanedaLuis@github.com>
2023-02-02 19:57:18 -0600

    GET: parámetros query, tomamos data desde npm faker

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const faker = require('faker');
+
 
 const app = express();
 
@@ -13,17 +15,34 @@ app.get('/nueva',( req, res) =>{
 });
 
 app.get('/products',( req, res) =>{
-    res.json([
-        {
-            name:'Camisa',
-            price: 1200
-        },
-        {
-            name:'Pantalon',
-            price: 800 
-        }
-    ]);
+    const products = []
+    const { size } = req.query;
+    const limit = size || 10
+
+    for (let index = 0; index < limit; index++) {
+        products.push({
+            name: faker.commerce.productName(),
+            price: parseInt(faker.commerce.price(),10),
+            image: faker.image.imageUrl(),
+        })
+        
+    }
+    res.json(products);
 });
+
+app.get('/users', (req, res) =>{
+    const { limit, offset } = req.query;
+    if( limit && offset){
+        res.json({
+            limit,
+            offset
+        })
+    }else{
+        res.send('No hay parametros')
+    }
+})
+
+
 
 app.get('/products/:id', (req, res)=>{
     const { id } = req.params;
@@ -33,6 +52,7 @@ app.get('/products/:id', (req, res)=>{
         price: 1200
     },)
 })
+
 
 app.get('/categories/:categoryId/products/:productId', (req, res) =>{
     const { categoryId, productId } = req.params;
