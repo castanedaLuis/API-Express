9e10017eb5724b97d32734c3393aeb9ece046c0b
castanedaLuis <castanedaLuis@github.com>
2023-02-02 19:24:13 -0600

    Routing con Express.js

--- index.js
+++ index.js
@@ -5,8 +5,19 @@ const app = express();
 const port = 3006;
 
 app.get('/',( req, res) =>{
-    res.send('Hola desde mi primer server');
+    res.json('Hola desde mi primer server');
+});
 
+app.get('/nueva',( req, res) =>{
+    res.json('Hola soy un nuevo endpoint');
+});
+
+app.get('/products',( req, res) =>{
+    res.json({
+        id:3,
+        name:'Camisa',
+        price: 1200
+    });
 });
 
 app.listen(port,()=>{
