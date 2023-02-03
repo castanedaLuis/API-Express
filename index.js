1b15b765803e09e60517136c4bb93c3ede990c9b
castanedaLuis <castanedaLuis@github.com>
2023-02-02 19:20:30 -0600

    Instalación de Express.js y creando el server

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('my app');
+const express = require('express');
+
+const app = express();
+
+const port = 3006;
+
+app.get('/',( req, res) =>{
+    res.send('Hola desde mi primer server');
+
+});
+
+app.listen(port,()=>{
+    console.log(`Listening at http://localhost:${port}`);
+});
