ff0e9068a64b1b29ef0176414261930a6699037b
castanedaLuis <castanedaLuis@github.com>
2023-02-02 20:13:25 -0600

    Separación de responsabilidades con express.Router

--- index.js
+++ index.js
@@ -1,67 +1,21 @@
 const express = require('express');
-const faker = require('faker');
-
+const routerApi = require('./routes');
 
 const app = express();
-
 const port = 3006;
 
-app.get('/',( req, res) =>{
-    res.json('Hola desde mi primer server');
-});
+app.get('/',(req,res)=>
+    {
+        res.send('Hola mi server en express');
+    });
 
-app.get('/nueva',( req, res) =>{
-    res.json('Hola soy un nuevo endpoint');
-});
+app.get('/nueva-ruta',(req,res)=>
+    {
+        res.send('Hola, soy una nueva ruta');
+    });
 
-app.get('/products',( req, res) =>{
-    const products = []
-    const { size } = req.query;
-    const limit = size || 10
+routerApi(app);
 
-    for (let index = 0; index < limit; index++) {
-        products.push({
-            name: faker.commerce.productName(),
-            price: parseInt(faker.commerce.price(),10),
-            image: faker.image.imageUrl(),
-        })
-        
-    }
-    res.json(products);
-});
-
-app.get('/users', (req, res) =>{
-    const { limit, offset } = req.query;
-    if( limit && offset){
-        res.json({
-            limit,
-            offset
-        })
-    }else{
-        res.send('No hay parametros')
-    }
-})
-
-
-
-app.get('/products/:id', (req, res)=>{
-    const { id } = req.params;
-    res.json({
-        id,
-        name:'Camisa',
-        price: 1200
-    },)
-})
-
-
-app.get('/categories/:categoryId/products/:productId', (req, res) =>{
-    const { categoryId, productId } = req.params;
-    res.json({
-        categoryId,
-        productId
-    })
-})
-
-app.listen(port,()=>{
-    console.log(`Listening at http://localhost:${port}`);
+app.listen(port,() => {
+    console.log('http://localhost:'+port);
 });
